97eb5820f5585902d1db6620a1c28d6ead25bcbe
Hikita1337 <33549201+Hikita1337@users.noreply.github.com>
2025-11-25 23:10:26 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-mkdir -p dumps/files
-mkdir -p dumps/ws
-mkdir -p dumps/requests
-mkdir -p dumps/responses
 
-mitmweb \
-  --listen-host 0.0.0.0 \
-  --listen-port 8080 \
-  --web-host 0.0.0.0 \
-  --web-port 8081 \
-  -s mitm_addons/mitm_logger.py
+echo "Installing mitmproxy…"
+pip install mitmproxy
+
+echo "Starting mitmdump (no GUI)…"
+mkdir -p dumps
+
+mitmdump --listen-host 0.0.0.0 --listen-port 8080 -s mitm_addons/mitm_logger.py
