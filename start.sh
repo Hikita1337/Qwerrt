be35b2e784afc5e663add2bd707e444edcf02e8c
Hikita1337 <33549201+Hikita1337@users.noreply.github.com>
2025-11-25 23:40:02 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Installing mitmproxy…"
-pip install mitmproxy
+echo "Installing deps…"
+pip install -r requirements.txt
 
-echo "Starting mitmdump (no GUI)…"
+echo "Starting mitmdump…"
 mkdir -p dumps
 
 mitmdump --listen-host 0.0.0.0 --listen-port 8080 -s mitm_addons/mitm_logger.py
